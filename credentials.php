7c39ae37242f5d0804c2ae401db016799b52a392
Alonso Vera <raalveco@gmail.com>
2026-04-19 23:42:11 -0600

    Phong Credentials

--- credentials.php
+++ credentials.php
@@ -3,8 +3,8 @@ declare(strict_types=1);
 
 return [
 	'baseUrl' => 'https://vara.e-sim.org/index.html',
-	'username' => 'Darius Grimsson',
-	'password' => 'darius123',
+	'username' => 'Phong Xiang',
+	'password' => 'phong123',
 	'userId' => '',
 	'muId' => '',
 	'allowAutoRegistration' => true,
